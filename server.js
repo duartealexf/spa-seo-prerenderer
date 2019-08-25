febbfab5526c6d3dd3eb01eabc81290dfbc2ce39
Alexandre Duarte <duartealexf@gmail.com>
2019-08-25 19:51:23 -0300

    test: fix travis.yml file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 /**
  * Based on https://developers.google.com/web/tools/puppeteer/articles/ssr
@@ -7,9 +7,8 @@ import puppeteer from 'puppeteer';
 const RENDER_CACHE = new Map();
 
 async function ssr(url) {
-  url = 1;
   if (RENDER_CACHE.has(url)) {
-    return {html: RENDER_CACHE.get(url), ttRenderMs: 0};
+    return { html: RENDER_CACHE.get(url), ttRenderMs: 0 };
   }
 
   const start = Date.now();
@@ -20,11 +19,11 @@ async function ssr(url) {
     // networkidle0 waits for the network to be idle (no requests for 500ms).
     // The page's JS has likely produced markup by this point, but wait longer
     // if your site lazy loads, etc.
-    await page.goto(url, {waitUntil: 'networkidle0'});
-    await page.waitForSelector('#posts'); // ensure #posts exists in the DOM.
+    await page.goto(url, { waitUntil: "networkidle0" });
+    await page.waitForSelector("#posts"); // ensure #posts exists in the DOM.
   } catch (err) {
     console.error(err);
-    throw new Error('page.goto/waitForSelector timed out.');
+    throw new Error("page.goto/waitForSelector timed out.");
   }
 
   const html = await page.content(); // serialized HTML of page DOM.
@@ -35,7 +34,7 @@ async function ssr(url) {
 
   RENDER_CACHE.set(url, html); // cache rendered page.
 
-  return {html, ttRenderMs};
+  return { html, ttRenderMs };
 }
 
-export {ssr as default};
+export { ssr as default };
