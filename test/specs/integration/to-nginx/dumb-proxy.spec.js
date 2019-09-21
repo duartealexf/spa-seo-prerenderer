780d0d86fc623ab2858cd9796923a72be7ef4761
Alexandre Duarte <duartealexf@gmail.com>
2019-09-21 13:55:39 -0300

    test: add tests with Nginx proxy

--- test/specs/integration/to-nginx/dumb-proxy.spec.js
+++ test/specs/integration/to-nginx/dumb-proxy.spec.js
@@ -2,15 +2,29 @@ const { describe, it } = require('mocha');
 const { assert } = require('chai');
 const cheerio = require('cheerio');
 
-const {
-  createDumbProxyHttpGetRequest,
-  requestSmartProxyDecidedToPrerender,
-} = require('../../../client');
+const { createDumbProxyHttpGetRequest, requestPassedThroughDumbProxy } = require('../../../client');
 
 describe('prerender requests to NodeJS behind dumb Nginx proxy', () => {
-  it('should receive prerendered index.html.', async () => {
+  it('should pass through dumb proxy with bot user agent.', async () => {
+    const { request } = await createDumbProxyHttpGetRequest('index.html', {}, true);
+    assert.isTrue(requestPassedThroughDumbProxy(request));
+  });
+
+  it('should pass through dumb proxy with non-bot user agent.', async () => {
+    const { request } = await createDumbProxyHttpGetRequest('index.html', {}, false);
+    assert.isTrue(requestPassedThroughDumbProxy(request));
+  });
+
+  it('should pass through dumb proxy and prerender.', async () => {
     const { response } = await createDumbProxyHttpGetRequest('index.html', {}, true);
     const $ = cheerio.load(response.body);
     assert.equal($('#app').length, 1);
+  });
+
+  it('should pass through dumb proxy and not prerender.', async () => {
+    const { response } = await createDumbProxyHttpGetRequest('index.html', {}, false);
+    const $ = cheerio.load(response.body);
+    assert.equal($('#app').length, 0);
+    assert.equal($('body').length, 1);
   });
 });
--- test/specs/integration/to-nginx/smart-proxy.spec.js
+++ test/specs/integration/to-nginx/smart-proxy.spec.js
@@ -5,16 +5,30 @@ const cheerio = require('cheerio');
 const {
   createSmartProxyHttpGetRequest,
   requestSmartProxyDecidedToPrerender,
+  requestPassedThroughSmartProxy,
 } = require('../../../client');
 
 describe('prerender requests to NodeJS behind smart Nginx proxy', () => {
-  it('should receive prerendered index.html.', async () => {
-    const { response } = await createSmartProxyHttpGetRequest('index.html', {}, true);
+  it('should pass through smart proxy with bot user agent.', async () => {
+    const { request } = await createSmartProxyHttpGetRequest('index.html', {}, true);
+    assert.isTrue(requestPassedThroughSmartProxy(request));
+  });
+
+  it('should pass through smart proxy with non-bot user agent.', async () => {
+    const { request } = await createSmartProxyHttpGetRequest('index.html', {}, false);
+    assert.isTrue(requestPassedThroughSmartProxy(request));
+  });
+
+  it('should pass through smart proxy and prerender.', async () => {
+    const { request, response } = await createSmartProxyHttpGetRequest('index.html', {}, true);
+    assert.isTrue(requestSmartProxyDecidedToPrerender(request));
+
     const $ = cheerio.load(response.body);
     assert.equal($('#app').length, 1);
   });
-  it('should pass through smart proxy.', async () => {
-    const { request } = await createSmartProxyHttpGetRequest('index.html', {}, true);
-    assert.isTrue(requestSmartProxyDecidedToPrerender(request));
+
+  it('should pass through smart proxy and not prerender.', async () => {
+    const { request } = await createSmartProxyHttpGetRequest('index.html', {}, false);
+    assert.isFalse(requestSmartProxyDecidedToPrerender(request));
   });
 });
